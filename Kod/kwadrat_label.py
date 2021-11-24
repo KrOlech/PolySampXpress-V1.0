47465ff3fc594b3653605ad697255a430eef882e
KrOlech <93384141+KrOlech@users.noreply.github.com>
2021-11-24 16:19:00 +0100

    Update kwadrat_label.py

--- Kod/kwadrat_label.py
+++ Kod/kwadrat_label.py
@@ -4,12 +4,20 @@ from PyQt5.QtCore import *
 
 class simple_obraz(QLabel):
 
-    def __init__(self, img, *args, **kwargs):
+    def __init__(self, img, Rectagle, *args, **kwargs):
         super(QWidget, self).__init__(*args, **kwargs)
         
-        self.wgraj(img)
+        self.Rectagle = Rectagle
+        
+        Rectagl = []
+        Rectagl.append(Rectagle[0]/6.36)
+        Rectagl.append(Rectagle[1]/6.72)
+        Rectagl.append(Rectagle[2]/6.33)
+        Rectagl.append(Rectagle[3]/6.53)
+       
+        self.wgraj(img, Rectagl)
 
-    def wgraj(self, img):
+    def wgraj(self, img, Rect):
     
         #wgranie obrazu
         self._img = img[0]
@@ -17,36 +25,19 @@ class simple_obraz(QLabel):
         #odczyt wymiarw zaczytanego obrazu
         h0 = img[0].size().height()
         w0 = img[0].size().width()
-
-        #miniaturyzacja wspurzednych pod
-        xHigh = h0#int(w0/h0*100)
-        yHigh = w0#int(h0/w0*100)
-
-        #scalpowanei grnego zaznaczonego pkt
-        x1 = int(img[2]*xHigh/h0)
-        y1 = int(img[1]*yHigh/w0)
-        xy1 = QPoint(10,10)
-
-        h0 = self.size().height()
-        w0 = self.size().width()
         
-        #scalowanie dolnego pkt
-        x2 = int((img[2]+img[4])*xHigh/h0)
-        y2 = int((img[1]+img[3])*yHigh/w0)
-        xy2 = QPoint(300,150)
-
-        self.rectangle = QRect(xy1,xy2)
-
+        self.rectangle = QRect(QPoint(Rect[0],Rect[1]),QPoint(Rect[2],Rect[3]))
         
-        yHigh = 310#int(w0/h0*100)*2
-        xHigh = 160#int(h0/w0*100)*2
-
         #wgranie obrazu
         self.setPixmap(self._img)
 
-        #zafixowanie rozmiaru poglondu
-        self.setMinimumSize(yHigh,xHigh)
-        self.setMaximumSize(yHigh,xHigh)
+    def update_rectagle(self,x,y,z,s):
+        Rect = []
+        Rect.append(self.Rectagle[0]/x)
+        Rect.append(self.Rectagle[1]/y)
+        Rect.append(self.Rectagle[2]/z)
+        Rect.append(self.Rectagle[3]/s)
+        self.rectangle = QRect(QPoint(Rect[0],Rect[1]),QPoint(Rect[2],Rect[3]))
 
     def paintEvent(self, QPaintEvent):
         # inicializacja pintera
@@ -63,20 +54,24 @@ class simple_obraz(QLabel):
 
         qp.drawRect(self.rectangle)
 
+        
+        
+
 class podglond_roi(QWidget):
 
     def __init__(self, text, img, obiekt_oznaczony, *args, **kwargs):
 
         super(QWidget, self).__init__(*args, **kwargs)
+        
+        
+        self.obiekt_oznaczony = obiekt_oznaczony
 
         ######################################################################################
         ##################################Przyciski i labele##################################
         ######################################################################################
 
-        #przyciski w ramach podglondu
-        self.butons = [QPushButton() for _ in range(4)]
         
-        self.podglond = simple_obraz(img)
+        self.podglond = simple_obraz(img,obiekt_oznaczony.get_wzgledny_rectagle())
         
         self.name_lable = QLabel()    
         self.name_lable.setText(text)            
@@ -84,33 +79,168 @@ class podglond_roi(QWidget):
         ######################################################################################
         ##################################Leyout##############################################
         ###################################################################################### 
-
         
         self.buton_layout =  QHBoxLayout()
-        [self.buton_layout.addWidget(Value) for Value in self.butons]
-
-
+        
         self.secenduary_layout = QVBoxLayout()
         self.secenduary_layout.addWidget(self.name_lable)
         self.secenduary_layout.addLayout(self.buton_layout)
         
 
-        self.main_layout = QHBoxLayout()
+        self.main_layout = QVBoxLayout()
         self.main_layout.addWidget(self.podglond)
         self.main_layout.addLayout(self.secenduary_layout)
 
+        self.kierunkowelayout = QGridLayout()
+        self.main_layout.addLayout(self.kierunkowelayout)
+        
+        
         self.setLayout(self.main_layout)
-         
-        self.setMaximumSize(640,180)
-        self.setMinimumSize(640,180)
-
+        
+        self.main_butons()
+        
+        self.set_size()
+       
 
         ######################################################################################
         ##################################conected############################################
         ######################################################################################
 
-        nazwy = ['', "", "", 'dell']
+        
+    
+    def __del__(self):
+        self.obiekt_oznaczony = 0
 
+    def edit(self):
+        pass
+
+    def main_butons(self):
+        
+        self.butons = [QPushButton() for _ in range(3)]
+        
+        [self.buton_layout.addWidget(Value) for Value in self.butons]
+        
+        nazwy = ["edit", "fine edit", "del"]
+        
         [swich.setText(name) for name, swich in zip(nazwy, self.butons)]
          
-        self.butons[3].clicked.connect(obiekt_oznaczony.kill)
+        fun = [self.edit,self.fine_edit,self.dellite]
+        
+        [b.clicked.connect(f) for b,f in zip(self.butons,fun)]
+        
+    def calibration_butons(self):
+    
+        self.butons = [QPushButton() for _ in range(8)]
+        
+        [self.buton_layout.addWidget(Value) for Value in self.butons]
+        
+        nazwykalibracionmode = ['xp', "yp", "zp", 'sp',"xm","ym","zm","sm"]
+        
+        [swich.setText(name) for name, swich in zip(nazwy, self.butons)]
+        
+        self.x, self.y, self.z, self.s = 6.36,6.72,6.33,6.53
+        
+        funkalibracionmode = [self.xp,self.yp,self.zp,self.sp,self.xm,self.ym,self.zm,self.sm]
+        [b.clicked.connect(f) for b,f in zip(self.butons,fun)]
+
+    def fine_edit(self):#
+        [self.buton_layout.removeWidget(b) for b in self.butons]
+        self.butons = 0
+        
+        self._Direction_buttons()
+        self.set_size(180,240)
+
+    def _Direction_buttons(self):
+        self.kierunkowe = [QPushButton() for _ in range(4)]
+
+        [button.setMaximumWidth(50) for button in self.kierunkowe]
+
+        #nadanie nazw przyciska
+        nazwy = ['/\\', "<", ">", '\/']
+        [swich.setText(name) for name, swich in zip(nazwy, self.kierunkowe)]
+
+        #przypiecie fukcji do przycisków
+        fun = [lambda : None ,lambda : None,lambda : None,lambda : None]
+
+        [swich.clicked.connect(f) for f, swich in zip(fun, self.kierunkowe)]
+        
+        #dodanie do leyatów przyciskó kierunkowych
+        it = [3, 2, 4, 3]
+        jt = [2, 3, 3, 4]
+        [self.kierunkowelayout.addWidget(value, j, i) for j, i, value in zip(jt, it, self.kierunkowe)]
+    
+        
+        self.t = QPushButton()
+        self.t.setMaximumWidth(50)
+        self.t.setText("return")
+        self.t.clicked.connect(self.normalbutons)
+        self.kierunkowelayout.addWidget(self.t,2,2)
+        
+    def normalbutons(self):
+        [self.kierunkowelayout.removeWidget(b) for b in self.kierunkowe]
+        self.kierunkowe = 0
+        
+        self.kierunkowelayout.removeWidget(self.t)
+        
+        self.t = 0
+
+        self.main_butons()
+        self.set_size()
+        
+    def dellite(self):
+        self.obiekt_oznaczony.kill()
+        self.obiekt_oznaczony = 0
+
+    def set_size(self,x = 180,y = 180):
+        self.setMaximumSize(x,y)
+        self.setMinimumSize(x,y)
+        
+    def xp(self):
+        self.x += 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def yp(self):
+        self.y += 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def zp(self):
+        self.z += 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def sp(self):
+        self.s += 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+
+    def xm(self):
+        self.x -= 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def ym(self):
+        self.y -= 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def zm(self):
+        self.z -= 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+        
+    def sm(self):
+        self.s -= 0.001
+        self.podglond.update_rectagle(self.x, self.y, self.z, self.s)
+        print(self.x, self.y, self.z, self.s)
+        self.update()
+
+#
