005b766caee07ba36219ff7d8899114b90645a4f
PC <Krzystof_olech@interia.pl>
2021-12-21 00:02:18 +0100

    Komentarze i opisy

--- Kod/Map.py
+++ Kod/Map.py
@@ -15,20 +15,24 @@ class Map(ROI_maping):
     # wartosci owsetu aktualnego podgloadu
     #ofsety = 768*2
     #ofsetx = 1024
-    
+
+    #okreslenie flagi zeby wstempnie zabezpiczeyc zeby nie wykonac neiwłąsciwego przemiesczenia ROI
     move_to_point = False
 
 
     def __init__(self, img, main_window, *args, **kwargs):
         super(Map, self).__init__(main_window, *args, **kwargs)
 
-        # zapisanie nowego obrazu
+        #skalowanei nowego obrazu
         self.image_opencv = cv2.resize(img, self.rozmiar)
-        
+
+        #zapisanie nowego obrazu
         img = self.image_opencv
-        
+
+        #ustawienie rozmiaru okna
         self.resize(self.rozmiar[0], self.rozmiar[1])
 
+        #konwersja obrazu z openCV na Qimage
         self.img = QImage(img, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888)
 
         #QImage(cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888)
@@ -38,14 +42,19 @@ class Map(ROI_maping):
         self.setPixmap(self.img)
 
     def new_image(self, img):
+        '''
+        Metoda przyjmujaca nowy obraz open CV konwertujaca go na Qimage i zaposujaca
+        :param img: open CV image
+        '''
 
-        #zapisanie nowego obrazu
-        self.image_opencv =  cv2.resize(img, self.rozmiar)
+        #skalowanie obrazu
+        self.image_opencv = cv2.resize(img, self.rozmiar)
 
+        #konwersja obrazu na Qimage
         self.img = QImage(img, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888)
-        #QImage(cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888)
-        self.img = QPixmap.fromImage(self.img)
 
+        #zapisanei obrazu
+        self.img = QPixmap.fromImage(self.img)
 
 
 class Map_window(QWidget):
@@ -59,17 +68,25 @@ class Map_window(QWidget):
         self.setWindowTitle("Mapa Próbki")
         self.setWindowIcon(QtGui.QIcon('icon.png'))
 
+        #ustawienie rozmiaru
         self.setGeometry(0, 0, 1024, 720)
         self.resize(self.rozmiar[0],self.rozmiar[1])
+
+        #stworzenie leyoutu
         self.layout = QVBoxLayout()
 
         #map object
         self.map = Map(map, main_window)
-        
-        self.resize(self.height(), self.width())
+
+        #dodanie mapy do podglondu
         self.layout.addWidget(self.map, Qt.AlignCenter)
+
+        #ustawienie layoputu
         self.setLayout(self.layout)
 
-    #pass object to map object
     def new_image(self, img):
+        '''
+        Mtoda przekzujaca nowa mape do ROI'u
+        :param img: nowa mapa klasy openCV
+        '''
         self.map.new_image(img)
