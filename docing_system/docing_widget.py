b13da9acf73e32f32ca0d0ebe5d3f692769d937d
krzysztof <krzysztof_olech@interia.pl>
2022-02-11 16:07:49 +0100

    docing_widget.py fundatio for docing gui

--- docing_system/docing_widget.py
+++ docing_system/docing_widget.py
@@ -3,14 +3,40 @@ from PyQt5.QtCore import QRect
 
 class Dock_in(QWidget):
 
-    def __init__(self,width, hight,*args, **kwargs):
+    def __init__(self, width, height, *args, **kwargs):
         super(Dock_in, self).__init__(*args, **kwargs)
 
 
-        self.setGeometry(QRect(width,hight))
+        self.setMaximumWidth(width)
+        self.setMaximumHeight(height)
+
+        self.setMinimumWidth(width)
+        self.setMinimumHeight(height)
+
 
 
 
 
 if __name__ == '__main__':
-    print("hwdp")
+
+    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtWidgets import QMainWindow
+    import sys
+    # stworzenie aplikacji
+    app = QApplication(sys.argv)
+
+    try:
+        # stworzenie glownego okna programu
+        okno = QMainWindow()
+
+        # wyswietlenie glownego okna
+        okno.show()
+
+        okno.setCentralWidget(Dock_in(100,100))
+
+        # wykonanie aplikacji
+        app.exec_()
+
+    except Exception as e:
+        # wypisanie bledu
+        print(e)
