58e54a182c5a943a9e6d50c6c3ca528cf5ea11b6
krzysztof <krzysztof_olech@interia.pl>
2022-02-11 16:14:34 +0100

    docing_widget.py fundatio for docing gui

--- docing_system/docing_widget.py
+++ docing_system/docing_widget.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import QRect
+
 
 class Dock_in(QWidget):
 
@@ -13,8 +13,8 @@ class Dock_in(QWidget):
         self.setMinimumWidth(width)
         self.setMinimumHeight(height)
 
-
-
+    def dock(self):
+        pass
 
 
 if __name__ == '__main__':
